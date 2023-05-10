fbc703d95958164580ab1f584ba3f175c265675b
Lean Marqs <leanmarqs@gmail.com>
2023-05-09 23:22:08 -0300

    All functions implemented and hyperparameters calibrated.

--- functions.py
+++ functions.py
@@ -21,7 +21,7 @@ def prepare_data(input_file):
     features = data[['Pclass', 'Sex', 'Age', 'Siblings/Spouses Aboard', 'Parents/Children Aboard', 'Fare']].values
     targets = data['Survived'].values
     values = data['Name'].values
-    weights = rg.random((1, len(values)))[0]
+    weights = rg.random((1, features.shape[1]))[0]
     data_final = pd.DataFrame(features,
                               columns=['Pclass', 'Sex', 'Age', 'Siblings/Spouses Aboard', 'Parents/Children Aboard',
                                        'Fare'])
@@ -33,3 +33,38 @@ def prepare_data(input_file):
 def save_data(data, output_file):
     with open(output_file, 'w') as f:
         f.write(data.to_string())
+
+
+# the np.dot function works make a sum of each ((feature * weight) + bias)
+def get_weighted_sum(feature, weights, bias):
+    return np.dot(feature.astype(float), weights) + bias
+
+
+# def sigmoid(w_sum):
+#     return 1 / (1 + np.exp(-w_sum))
+
+# the clip function maintain the w_sum size among -500 and 500, preventing overflow with a large w_sum to exp.
+def sigmoid(w_sum):
+    clipped_sum = np.clip(w_sum, -500, 500)
+    return 1 / (1 + np.exp(-clipped_sum))
+
+
+# def cross_entropy(target, prediction):
+#     return -(target * np.log10(prediction) + (1 - target) * np.log10(1 - prediction))
+
+# a small constant was added to prevent log10(0)
+def cross_entropy(target, prediction):
+    epsilon = 1e-10
+    return -(target * np.log10(prediction + epsilon) + (1 - target) * np.log10(1 - prediction + epsilon))
+
+
+def update_weights(weights, l_rate, target, prediction, feature):
+    new_weights = []
+    for x, w in zip(feature, weights):
+        new_w = w + l_rate * (target - prediction) * x
+        new_weights.append(new_w)
+    return new_weights
+
+
+def update_bias(bias, l_rate, target, prediction):
+    return bias + l_rate * (target - prediction)
--- main.py
+++ main.py
@@ -1,4 +1,48 @@
-from functions import generate_data, prepare_data, save_data
+import numpy as np
+import matplotlib.pyplot as plt
+
+from functions import generate_data, prepare_data, save_data, get_weighted_sum, sigmoid, cross_entropy, update_bias, \
+    update_weights
+
+# save_data(data, 'dataset/titanic_data.txt')
+
+bias = 0.5
+l_rate = 0.0005
+epochs = 150
+epoch_loss = []
 
 data, weights = prepare_data('dataset/titanic_original.csv')
-save_data(data, 'dataset/titanic_data.txt')
+
+
+def train_model(data, weights, bias, l_rate, epochs):
+    for e in range(epochs):
+        individual_loss = []
+        for i in range(len(data)):
+            # the [:-2] select the first 6 cols of the table data, where we find the features.
+            feature = data.loc[i][:-2]
+            # the [-1] select the very last col of the table data, where we find the targets.
+            target = data.loc[i][-1]
+            w_sum = get_weighted_sum(feature, weights, bias)
+            prediction = sigmoid(w_sum)
+            loss = cross_entropy(target, prediction)
+            individual_loss.append(loss)
+            # gradient descent
+            weights = update_weights(weights, l_rate, target, prediction, feature)
+            bias = update_bias(bias, l_rate, target, prediction)
+        average_loss = sum(individual_loss) / len(individual_loss)
+        epoch_loss.append(average_loss)
+        print('**********************************')
+        print('epoch ', e)
+        print(average_loss)
+
+# plot epoch_loss
+    plt.plot(np.arange(epochs), epoch_loss)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Epoch Loss')
+    plt.show()
+
+
+train_model(data, weights, bias, l_rate, epochs)
+
+
